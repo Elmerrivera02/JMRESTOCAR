f6c8024d2aa62af2fac58b8265d8af854df85532
Elmer.R <riverajair195@gmail.com>
2024-11-27 16:28:53 -0500

    modificacion

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login y Registro</title>
+    <title>Login y </title>
     <link rel="stylesheet" href="CSS/estilos.css">
 </head>
 <body>
